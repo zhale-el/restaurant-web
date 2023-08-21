b6bdcad73aa80c534abc17de3fa2efdb0ac4c9e1
zhaleh <zhalehelmi@gmail.com>
2023-08-21 15:26:17 +0330

    write html nav menu

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// < ====== Show menu ====== >
+
+//validate that variables exist
+
+// we add the show-menu class the div tag the nav__mene claa
+
+// < ====== Remove menu mobile ======= >
+
+// when we click on each nav__link, we remove the show-menu class
+
+// < ====== Scroll sections active link ====== >
+
+// < ====== Change background header ====== >
+
+// when the scoll is greater than 200 viewport height, add the scoll-header class
