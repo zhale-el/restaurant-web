6462954f7746cc050eba36b23bc55568b1807e4c
zhaleh <zhalehelmi@gmail.com>
2023-08-22 12:41:25 +0330

    js scrolltop

--- js/script.js
+++ js/script.js
@@ -58,3 +58,32 @@ function scrollHeader() {
 }
 
 window.addEventListener("scroll", scrollHeader);
+
+// < ====== Show scroll top ====== >
+
+function scrollTop() {
+  const scrollTop = document.getElementById("scroll-top");
+  //when the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 500) scrollTop.classList.add("show-scroll");
+  else scrollTop.classList.remove("show-scroll");
+}
+
+window.addEventListener("scroll", scrollTop);
+
+/*==================== DARK LIGHT THEME ====================*/
+
+// Previously selected topic (if user selected)
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+
+// We validate if the user previously chose a topic
+
+// If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+
+// Activate / deactivate the theme manually with the button
+
+// Add or remove the dark / icon theme
+
+// We save the theme and the current icon that the user chose
+
+/*==================== SCROLL REVEAL ANIMATION ====================*/
